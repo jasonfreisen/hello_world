9a944ff8ce3a46f7b520e3478477da798704b6af
jasonfreisen <jaf3457@miami.edu>
2025-09-04 10:12:32 -0400

    hello world r script

---
+++ scripts/03_content/hello_world.R
@@ -0,0 +1,17 @@
+#build a graph
+
+#load packages
+library(ggplot2)
+library(EVR628tools)
+
+#load data
+data("data_heatwaves")
+
+#build a plot
+p <- ggplot(data = data_sst |> head(365),
+       mapping = aes(x = date, y = temperature_C)) +
+  geom_line()
+
+#export my plot
+ggsave(plot = p,
+       filename = "results/img/hello_world")
